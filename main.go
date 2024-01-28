43c5dfbc31f10fd6f0443584e475a0168bbb9722
ZeroHzzzz <3190381602@qq.com>
2024-01-29 00:21:35 +0800

    Update: 1.29: Modified main.go

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"hr/configs/router"
 	"log"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,20 +11,8 @@ func main() {
 	// database.Init()
 
 	r := gin.Default()
-	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "authorization, Content-Type")
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-			return
-		}
-
-		c.Next()
-	})
-
-	// router.Init(r)
+	router.Init(r)
 	err := r.Run(":3000")
 	if err != nil {
 		log.Fatal("Server start error", err)
