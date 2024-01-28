44eb036bde43183aa8b647c8303b9e08f2c50938
ZeroHzzzz <3190381602@qq.com>
2024-01-29 00:17:48 +0800

    Update 1.29: Add router.go

--- configs/router/router.go
+++ configs/router/router.go
@@ -28,15 +28,67 @@ func Init(r *gin.Engine) {
 		login.GET("/student")
 		login.GET("/counsellor")
 	}
+
 	const preStudent = "/student"
 	student := r.Group(preStudent)
 	{
 		student.Use(func(c *gin.Context) {
-			midware.AuthenticateMiddleware(c, "admin", "student")
+			midware.AuthenticateMiddleware(c, "counsellor", "student")
 			if !c.IsAborted() {
 				c.Next()
 			}
 		})
-		student.PUT("/profile")
+
+		student.PUT("/profile/:userId")
+		student.GET("/profile/:userId")
+
+		student.POST("/:userId/form/submit")
+		student.GET("/:userId/form/search")
+
+		student.POST("/feedback")
+		student.POST("/recommend")
+
+		student.GET("/score/:userId/:academicYear")
+	}
+
+	const preCounsellor = "/counsellor"
+	counsellor := r.Group(preCounsellor)
+	{
+		counsellor.Use(func(c *gin.Context) {
+			midware.AuthenticateMiddleware(c, "counsellor")
+			if !c.IsAborted() {
+				c.Next()
+			}
+		})
+
+		counsellor.GET("/audit/pending")
+		counsellor.GET("/audit/:formId")
+		counsellor.PUT("/audit/:formId/review")
+		counsellor.PUT("/audit/review")
+		counsellor.GET("/audit/:formId/history")
+		counsellor.DELETE("/audit/:formId/revoke")
+
+		counsellor.PUT("/grade/personal-import/:userId")
+		counsellor.PUT("/grade/bulk-import")
+
+		counsellor.POST("/:counsellorId/cause")
+		counsellor.POST("/setddl")
+	}
+
+	const preSquare = "square"
+	square := r.Group(preSquare)
+	{
+		square.Use(func(c *gin.Context) {
+			midware.AuthenticateMiddleware(c, "counsellor", "student")
+			if !c.IsAborted() {
+				c.Next()
+			}
+		})
+		square.POST("/topic/create")
+		square.GET("/topic")
+		square.GET("/square/topic/:topicId")
+		square.POST("/topic/:topicId/replies")
+		square.PUT("/topic/:topicId")
+		square.DELETE("/topic/:topicId")
 	}
 }
