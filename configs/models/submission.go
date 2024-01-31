676353684f1e9ed2b856de141f9ef6844fef9f56
ZeroHzzzz <3190381602@qq.com>
2024-02-01 02:10:24 +0800

    Update 2.1: Modified submission data models

--- configs/models/submission.go
+++ configs/models/submission.go
@@ -1,9 +1,22 @@
 package models
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type SubmitInformation struct {
 	CurrentUser
-	ItemName     string
-	AcademicYear string
-	Evidence     []string
-	Status       bool
+	ItemName     string   `bson:"itemName"`
+	AcademicYear string   `bson:"academicYear"`
+	Evidence     []string `bson:"evidence"`
+	Status       bool     `bson:"status"`
+}
+
+type SubmitHistory struct {
+	ID           primitive.ObjectID `bson:"_id,omitempty"`
+	SubmissionId string             `bson:"submissionId"`
+	AuditorId    string             `bson:"auditorId"`
+	CreateAt     time.Time          `bson:"create_at"`
 }
